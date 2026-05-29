e4539e29a015b1b3e70a2ca6a08d6280af78e50f
Aeri4a <80831299+Aeri4a@users.noreply.github.com>
2026-05-29 22:55:50 +0200

    added webhook sender

--- server/internal/notifier/sender_webhook.go
+++ server/internal/notifier/sender_webhook.go
@@ -1,13 +1,50 @@
 package notifier
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
+	"log"
+	"net/http"
 	"server/internal/analyzer"
 	"server/internal/database"
+	"strings"
 )
 
 type WebhookSender struct{}
 
+type WebhookPayload struct {
+	Agent   string `json:"agent"`
+	Message string `json:"message"`
+}
+
 func (sen *WebhookSender) SendAlert(ctx context.Context, channel database.NotificationChannel, report *analyzer.AnalysisReport) error {
+	url := channel.Target
+
+	payload := WebhookPayload{
+		Agent:   "",
+		Message: strings.Join(report.Findings, "\n"),
+	}
+
+	jsonPayload, err := json.Marshal(payload)
+	if err != nil {
+		return err
+	}
+
+	req, err := http.NewRequestWithContext(ctx, "POST", url, bytes.NewBuffer(jsonPayload))
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	log.Printf("Alert sent to [%s] channel.", channel.Name)
+
 	return nil
 }
